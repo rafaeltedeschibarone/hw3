15e3591915afdd129a5d600caf194c53d4a379cf
rafaeltedeschibarone <rafael.barone@kellogg.northwestern.edu>
2026-02-24 20:43:35 -0600

    Add routes for places and entries in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+
+  get("/", { :controller => "places", :action => "index" })
+
+  # Places
+  get("/places", { :controller => "places", :action => "index" })
+  get("/places/new", { :controller => "places", :action => "new" })
+  post("/places", { :controller => "places", :action => "create" })
+  get("/places/:id", { :controller => "places", :action => "show" })
+
+  # Entries
+  get("/entries/new/:place_id", { :controller => "entries", :action => "new" })
+  post("/entries", { :controller => "entries", :action => "create" }) 
+  
 end
